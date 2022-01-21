4b84a9cd7809bbafdc1c8e0c1d031b68b51e64ce
GuillermoTejera <guilletejera@gmail.com>
2022-01-21 20:07:08 -0300

    Mejora de excepciones

--- api/Archivos.php
+++ api/Archivos.php
@@ -30,17 +30,39 @@ abstract class Archivos {
                 }
             }
 
-            $_SESSION["cantPersonasRegistradas"]  = utf8_encode($data[0][1]);
-            $_SESSION["promAniosRacing"] = 5000;
-            $_SESSION["nombresRiver"] = ["Guille","Guille1","Guille2","Guille3","Guille4"];
+            // TODO:
+            $_SESSION["cantPersonasRegistradas"]  = 50000;
+            $_SESSION["promAniosRacing"] = utf8_encode($data[0][1]);
+            $_SESSION["nombresRiver"] = ["Guille", "Guille1", "Guille2", "Guille3", "Guille4"];
+            $_SESSION["tablaPersonas"] = [[utf8_encode($data[0][0]), utf8_encode($data[0][1]), utf8_encode($data[0][2])], [utf8_encode($data[1][0]), utf8_encode($data[1][1]), utf8_encode($data[1][2])]];
+            $_SESSION["tablaEdades"] = [[500, "equipo", 22, 20, 24], [400, "equipo2", 22, 20, 24]];
         };
     }
 
     public static function getData(): void {
-        Response::getResponse()->appendData('cantPersonasRegistradas', $_SESSION["cantPersonasRegistradas"]);
-        Response::getResponse()->appendData('promAniosRacing', $_SESSION["promAniosRacing"]);
-        Response::getResponse()->appendData('nombresRiver', $_SESSION["nombresRiver"]);
-        //Response::getResponse()->appendData('tablaPersonas', $_SESSION["tablaPersonas"]);
-        //Response::getResponse()->appendData('tablaEdades', $_SESSION["tablaEdades"]);
+        if (isset($_SESSION["cantPersonasRegistradas"]))
+            Response::getResponse()->appendData('cantPersonasRegistradas', $_SESSION["cantPersonasRegistradas"]);
+        else
+            throw new Exception('Problema interno, intentelo nuevamente');
+
+        if (isset($_SESSION["promAniosRacing"]))
+            Response::getResponse()->appendData('promAniosRacing', $_SESSION["promAniosRacing"]);
+        else
+            throw new Exception('Problema interno, intentelo nuevamente');
+
+        if (isset($_SESSION["nombresRiver"]))
+            Response::getResponse()->appendData('nombresRiver', $_SESSION["nombresRiver"]);
+        else
+            throw new Exception('Problema interno, intentelo nuevamente');
+
+        if (isset($_SESSION["tablaPersonas"]))
+            Response::getResponse()->appendData('tablaPersonas', $_SESSION["tablaPersonas"]);
+        else
+            throw new Exception('Problema interno, intentelo nuevamente');
+
+        if (isset($_SESSION["tablaEdades"]))
+            Response::getResponse()->appendData('tablaEdades', $_SESSION["tablaEdades"]);
+        else
+            throw new Exception('Problema interno, intentelo nuevamente');
     }
 }
